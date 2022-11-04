1d39e663bedf18cfc2fb49c6341b4827b20bfa38
u776341 <marco.stenta@syngenta.com>
2022-11-04 13:30:01 +0100

    Bump version 0.4.3 -> 0.4.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.autosummary',
               'sphinx.ext.viewcode']
 
-version = "0.4.3"
+version = "0.4.4"
 # The full version, including dev info
 release = version.replace("_", "")
 
