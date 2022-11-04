272ee63ee3bfde170498eeac179c080d82838b62
u776341 <marco.stenta@syngenta.com>
2022-11-04 09:40:29 +0100

    bug version docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,22 +10,22 @@ project = 'MPSKit'
 copyright = '2022, Marco Stenta'
 author = 'Marco Stenta'
 
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+modindex_common_prefix = ["mpskit."]
+extensions = []
+
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.viewcode']
+
 version = "0.4.1"
 # The full version, including dev info
 release = version.replace("_", "")
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = 'bizstyle'
+html_static_path = ['../static']
+html_logo = "../static/mpskit_logo.png"
